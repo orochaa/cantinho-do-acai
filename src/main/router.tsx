8ab7297508651004f33c6dc9cd4e5ba218453487
Bruno Rocha <brunorocha2674@gmail.com>
2023-03-04 23:33:53 -0300

    fix: scroll navigation

--- src/main/router.tsx
+++ src/main/router.tsx
@@ -1,16 +1,19 @@
 import { Background } from '@/presentation/components'
 import { ComplementsPage, HomePage } from '@/presentation/pages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ScrollToTop } from './scrool-to-top'
 
 export function Router(): JSX.Element {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="" element={<Background />}>
-          <Route path="" element={<HomePage />} />
-          <Route path=":item" element={<ComplementsPage />} />
-        </Route>
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="" element={<Background />}>
+            <Route path="" element={<HomePage />} />
+            <Route path=":item" element={<ComplementsPage />} />
+          </Route>
+        </Routes>
+      </ScrollToTop>
     </BrowserRouter>
   )
 }
---
+++ src/main/scrool-to-top.tsx
@@ -0,0 +1,11 @@
+import { ReactNode, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+export function ScrollToTop(props: { children: ReactNode }): JSX.Element {
+  const location = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
+  return <>{props.children}</>
+}
