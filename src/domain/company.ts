3fa8847935d7ec5baac2c0150214afe97cf7efd7
Bruno Rocha <brunorocha2674@gmail.com>
2026-09-12 22:48:54 -0300

    chore: Update company address

--- src/domain/company.ts
+++ src/domain/company.ts
@@ -3,17 +3,33 @@ export interface CompanyCoordinates {
   readonly lon: number;
 }
 
+export interface CompanyAddress {
+  readonly street: string;
+  readonly number: string;
+  readonly neighborhood: string;
+  readonly city: string;
+  readonly state: string;
+}
+
 export interface CompanyInfo {
   readonly name: string;
   readonly whatsappPhone: string;
+  readonly address: CompanyAddress;
   readonly coordinates: CompanyCoordinates;
 }
 
 export const companyInfo: CompanyInfo = {
   name: 'Cantinho do Açaí',
   whatsappPhone: '5554984312998',
+  address: {
+    street: 'Rua Claudino Cirilo Zeni',
+    number: '27',
+    neighborhood: 'Charqueadas',
+    city: 'Caxias do Sul',
+    state: 'RS',
+  },
   coordinates: {
-    lat: -29.190_012_3,
-    lon: -51.213_029_1,
+    lat: -29.1894,
+    lon: -51.213_72,
   },
 };
--- src/pages/cart/components/cart-pickup-notice.tsx
+++ src/pages/cart/components/cart-pickup-notice.tsx
@@ -1,11 +1,15 @@
+import { companyInfo } from '@/domain/company';
+
 export function CartPickupNotice(): React.JSX.Element {
+  const { address } = companyInfo;
+
   return (
     <div className="rounded-sm bg-yellow-100 p-4 text-pretty text-yellow-800">
       <p className="font-semibold">Atenção:</p>
       <p>Retirar pedido no local, no endereço abaixo:</p>
       <p>
-        Endereço: Rua Olinda de Almeida Lima, 249 - Charqueadas, Caxias do Sul -
-        RS
+        Endereço: {address.street}, {address.number} - {address.neighborhood},{' '}
+        {address.city} - {address.state}
       </p>
     </div>
   );
